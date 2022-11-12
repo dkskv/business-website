dac12a19e1ca4cb7f6e5f2a1a6f03461bc2f1c9b
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-12 18:52:31 +0300

    feat: удалена страница с портфолио

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ServiceListComponent } from './service-list/service-list.component';
 import { ServiceRequestComponent } from './service-request/service-request.component';
 
 const routes: Routes = [
   { path: 'about-us', component: AboutUsComponent },
-  { path: 'portfolio', component: PortfolioComponent },
   { path: 'service-list', component: ServiceListComponent },
   { path: 'service-request', component: ServiceRequestComponent },
 ];
