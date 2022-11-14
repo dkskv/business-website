0debe9e632af2483a7bedf73b82022b88c5775c0
Dmitry Kuskov <dmtrkskv@gmail.com>
2022-11-15 00:27:37 +0300

    refactor: удалил неиспользуемый импорт

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -35,7 +34,6 @@ import { LayoutComponent } from './layout/layout.component';
     MatSelectModule,
     MatSnackBarModule,
     MatButtonModule,
-    MatToolbarModule,
     MatCardModule,
     MatIconModule,
     MatProgressBarModule
